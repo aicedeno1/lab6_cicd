7331c649908768bfca749492c5b4cd5c5a3ecd1d
aicedeno1 <aicedeno1@espe.edu.ec>
2026-07-16 11:50:00 -0500

    Preparar api para despliegue en vercel

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Integracion continua trabajando');
+app.use(express.json());
+
+app.get('/saludo/:nombre', (req, res) => {
+  const { nombre } = req.params;
+
+  res.status(200).json({
+    mensaje: `¡Hola, ${nombre}`,
+    estado: 'API disponible'
+  });
+
+});
+app.get('/suma', (req, res) => {
+  const numero1 = Number(req.query.numero1);
+  const numero2 = Number(req.query.numero2);
+
+  if(Number.isNaN(numero1) || Number.isNaN(numero2)) {
+    return res.status(400).json({
+      error: 'Los parametros numero 1 y numero 2 deben ser numericos'
+    });
+  }
+
+  res.status(200).json({
+    numero1,
+    numero2,
+    resultado: numero1 + numero2
+  });
+
 });
 
 app.listen(port, () => {
